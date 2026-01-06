04e33dd5a1cc7fc26fc57316fe85b0da41b7d8d9
Flixyop <108677630+Flixyop@users.noreply.github.com>
2026-01-06 13:39:44 +0000

    caca

---
+++ camera.py
@@ -0,0 +1,5 @@
+class Camera():
+    def init(self):
+        self.x = 30
+        self.y = 30
+        self.size = 100
--- utils.py
+++ utils.py
@@ -5,9 +5,16 @@ BOARD_SIZE = (35, 25)
 
 SCREEN_SIZE = [PIXEL_SIZE * BOARD_SIZE[0], PIXEL_SIZE * BOARD_SIZE[1]]
 
+world_width = 100
+world_height = 100
+
+village_coord = (30, 30)
+
+facteur = 
 
 # Assets
 
 VILLAGE_MAP_ASSET = "assets/map/map_village.png"
+VILLAGE_MAP_ASSET = pygame.transform.scale(VILLAGE_MAP_ASSET, )
 PLAYER_ASSET = "assets/entity/player/idle/1.png"
 GOBLIN_ASSET = "assets/entity/enemy/goblin/idle/1.png"
